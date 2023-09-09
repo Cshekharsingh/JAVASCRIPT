456ca76a9caadb03d045c26aa6e1434d82643201
Cshekharsingh <128500095+Cshekharsingh@users.noreply.github.com>
2023-09-09 13:56:09 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId =14453 
+let accountEmail = "chandrashekharmn5@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bhopal"
+let accountState ;
+//accountId = 2  Not allowed
+accountEmail = "rdmnote5@gmail.com"
+accountCity ="Indore"
+console.log(accountId);  
+//console.log(accountEmail);
+//console.log(accountCity);
+console.table([accountId ,accountEmail,accountCity,accountPassword,accountState]);

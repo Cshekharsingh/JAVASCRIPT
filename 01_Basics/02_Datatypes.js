6f7229de12e3c7605ff5e13bf4e229c7aefe3313
Cshekharsingh <128500095+Cshekharsingh@users.noreply.github.com>
2023-09-09 13:55:14 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,6 @@
+"Use strict";// treat all js code as newer version 
+/*alert("Hello")
+alert(3+3) We are using node js not browser*/
+let name = "Shekhar"
+console.log(3+3);
+console.log(typeof name);

70689dda71ab7bc272ce3ea20455d96d9f9572a6
Cshekharsingh <128500095+Cshekharsingh@users.noreply.github.com>
2023-09-09 14:18:39 +0000

    Conversion

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+let score = true
+
+console.log(typeof(score));
+let valueInNumber = Number (score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+let isLoggedIn = 1 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+let number1 = 33
+
+let StringNumber = String(number1)
+console.log(StringNumber);
+console.log(typeof(StringNumber));

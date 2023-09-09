9ab3a6e069bae00ebd9820b6d48f3dee237cff97
Cshekharsingh <128500095+Cshekharsingh@users.noreply.github.com>
2023-09-09 14:54:17 +0000

    Comparision

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -13,3 +13,15 @@ let number1 = 33
 let StringNumber = String(number1)
 console.log(StringNumber);
 console.log(typeof(StringNumber));
+
+// *************Operations***************//
+
+
+let value = 3 
+let negvalue = -value
+console.log(negvalue);
+
+let str1= "Chandra"
+let str2= " Shekhar"
+let str3 = str1+str2
+console.log(str3);

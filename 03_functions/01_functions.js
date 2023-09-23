a5dadbd333ced7d2f052823de180ecd9b170fa88
Cshekharsingh <128500095+Cshekharsingh@users.noreply.github.com>
2023-09-23 16:01:54 +0000

    Functions

---
+++ 03_functions/01_functions.js
@@ -0,0 +1,16 @@
+function saymyname()
+{
+    console.log("S");
+    console.log("H");
+    console.log("E");
+    console.log("K");
+    console.log("H");
+    console.log("A");
+    console.log("R");
+}
+saymyname()
+function sum(number1 , number2 ){
+    console.log(number1+number2);
+
+}
+sum(2,4)

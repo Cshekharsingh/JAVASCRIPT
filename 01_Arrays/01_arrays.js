e89e9ca0c10cf08c4c6d2dcbeb90632a2afd8c63
Cshekharsingh <128500095+Cshekharsingh@users.noreply.github.com>
2023-09-23 21:11:03 +0530

    Delete 01_Arrays directory

--- 01_Arrays/01_arrays.js
+++
@@ -1,7 +0,0 @@
-const myArr = [0,1,2,3,4,5]
-const Heros = ["SuperMan", "BatMan" ," CaptianAmerica" ]
-myArr.push(10)
-console.log(myArr);
-console.log(myArr.slice(1,3));
-console.log(myArr.splice(1,3));
-console.log(myArr);
--- 01_Arrays/02_arrays.js
+++
@@ -1,7 +0,0 @@
-const Hero = ["CaptainAmerica", " Flash", " SpiderMan ", "IronMan", "Thor ", " Hulk"]
-const Hero2 = ["Chitti" , "Shaktiman","Krish " ,"RaOne "]
-const Name = "Shekhar"
-const Number = "1234456"
-console.log(Hero.concat(Hero2));
-console.log(Array.from(Name));
-console.log(Array.from(Number));
